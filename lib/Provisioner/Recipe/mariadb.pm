34003948272e6ee34d024f9768a7b03616fc7bba
George Baugh <george@troglodyne.net>
2025-10-13 14:17:15 -0500

    Add mariadb target

---
+++ lib/Provisioner/Recipe/mariadb.pm
@@ -0,0 +1,57 @@
+package Provisioner::Recipe::mariadb;
+
+use strict;
+use warnings;
+
+use parent qw{Provisioner::Recipe};
+
+=head1 Provisioner::Recipe::mariadb
+
+=head2 SYNOPSIS
+
+    somedomain:
+        mariadb:
+            dump: path/to/dump/in/datadir
+            version: 
+            root_pw: blahblah
+
+=head2 DESCRIPTION
+
+Set up the specified mariadb version and install the provided dump.
+Secures the DB and sets the root pw as specified.
+
+=cut
+
+sub deps {
+	my ($self) = @_;
+	if ($self->{target_packager} eq 'deb') {
+		return qw{mariadb-client libmariadb-dev-compat libmariadb-dev mariadb-backup};
+	}
+	die "Unsupported packager";
+}
+
+sub validate {
+	my ($self, %opts) = @_;
+
+    my $rpw = $opts{root_pw};
+    die "Must define root_pw ns in [mariadb] section of recipes.yaml" unless $rpw;
+
+    my $dump = $opts{dump};
+    die "Must define dump in [mariadb] section of recipes.yaml" unless $dump;
+
+    my $ver = $opts{version};
+    die "Must define version in [mariadb] section of recipes.yaml" unless $ver;
+
+	return %opts;
+}
+
+sub template_files {
+	my ($self) = @_;
+
+	return (
+		'mysql.secure_installation.tt' => 'mysql_secure_installation.sql',
+        'my.cnf.tt'                    => 'my.cnf',
+    );
+}
+
+1;
