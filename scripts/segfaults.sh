38a42b6dfdc410835137bb5e77fccbedef81b11a
George S. Baugh <george@troglodyne.net>
2026-05-19 10:50:26 -0400

    suppress useless warning about syslog being a binary file

--- scripts/segfaults.sh
+++ scripts/segfaults.sh
@@ -11,12 +11,11 @@ else
     SYSLOG=/var/log/syslog
 fi
 
-
 touch /root/segfaults.log
 touch /root/new-segfaults.log
 mv /root/new-segfaults.log /root/segfaults.log
 FSZ=$(stat --printf "%s" /root/segfaults.log)
-grep -i 'segfault' $SYSLOG | grep -v 'segfaults.sh' >> /root/new-segfaults.log
+grep -i 'segfault' $SYSLOG 2>/dev/null | grep -v 'segfaults.sh' >> /root/new-segfaults.log
 echo "$(sort < /root/new-segfaults.log | uniq)" > /root/new-segfaults.log
 NEWSZ=$(stat --printf "%s" /root/new-segfaults.log)
 
